dfbb9704dc2a4159ba4019520ba8805db4de0c3b
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-06-12 16:11:26 +0200

    feat: add logger utility
    with a global configuration file

---
+++ src/lib/infrastructure/logger.ts
@@ -0,0 +1,28 @@
+import pino, { Logger, LoggerOptions } from 'pino';
+import pretty from 'pino-pretty';
+
+import { config } from '../config';
+
+const { logging } = config;
+
+let logger: Logger;
+const loggerOptions: LoggerOptions = {
+  level: logging.logLevel,
+  redact: ['req.headers.authorization'],
+  enabled: logging.enabled,
+};
+
+if (logging.logForHumans) {
+  const omitDay = 'HH:MM:ss';
+  const prettyPrint = pretty({
+    sync: true,
+    colorize: true,
+    translateTime: omitDay,
+    ignore: 'pid,hostname',
+  });
+  logger = pino(loggerOptions, prettyPrint);
+} else {
+  logger = pino(loggerOptions);
+}
+
+export { logger };
