9bda38e200e44cb98bfe574c98f158fdbf55b777
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-07-05 14:39:21 +0200

    feat: add create-audit-log.usecase
    Co-authored-by: Eric Lim <eric.lim@zenika.com>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- src/lib/infrastructure/repositories/audit-log-postgres.repository.ts
+++ src/lib/infrastructure/repositories/audit-log-postgres.repository.ts
@@ -1,9 +1,11 @@
 import { type AuditLog } from '../../domain/models/audit-log.ts';
 import { type AuditLogRepository } from '../../domain/interfaces/audit-log.repository.ts';
-import {knex} from '../../../db/knex-database-connection.ts';
+import { knex } from '../../../db/knex-database-connection.ts';
 
 export class AuditLogPostgresRepository implements AuditLogRepository {
   async create(auditLog: AuditLog): Promise<void> {
-    await knex('audit-log').insert(auditLog)
+    await knex('audit-log').insert(auditLog);
   }
 }
+
+export const auditLogPostgresRepository = new AuditLogPostgresRepository();
